d852ad1b7f37b5219ed18c3d2075984fa9dea76d
Victor Frolov <vicfrolov@gmail.com>
2015-10-24 20:30:32 -0700

    removed php OAUTh files, and added node OAUTH files

---
+++ front-end/twitterNPM.js
@@ -0,0 +1,13 @@
+var Twit = require('twit')
+ 
+var T = new Twit({
+    consumer_key:         'CO0QIryPxSTsxM5f94kzr8rby'
+  , consumer_secret:      'nMuFLNd460HcJzM7xgolu3MldodNb9CmpxUr55N7ubBEAcCOQg'
+  , access_token:         '35929248-6VzSZMtmRokPJqmwJ3QIWSLyzxAjJCJeHVOoyShH9'
+  , access_token_secret:  'oVfSUB613mtSgsfNdrg5KbLLtlHIXwSMDnmaXiKwfSh8R'
+})
+
+
+T.get('search/tweets', { q: 'beverly hills playhouse', count: 100}, function(err, data, response) {
+  console.log(data)
+})
