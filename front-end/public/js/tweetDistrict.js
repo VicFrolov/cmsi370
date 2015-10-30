22dd4ad154d2d5a6e81a2d7540226ab9f53509ef
Victor Frolov <vicfrolov@gmail.com>
2015-10-29 21:34:29 -0700

    dropping pins based on location for tweets with a lat/lon value in the map

--- front-end/public/js/tweetDistrict.js
+++ front-end/public/js/tweetDistrict.js
@@ -1,6 +1,15 @@
 $(function () {
 
     $("#search-button").click(function() {
+        // variables for google maps
+        var LatValue = parseFloat($("#searchLat").val());
+        var LonValue = parseFloat($("#searchLon").val());
+        var myLatLng = {lat: LatValue, lng: LonValue};
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'), {
+            zoom: 12,
+            center: myLatLng
+        });              
         $.getJSON(
             "http://localhost:3000/tw",
 
@@ -12,38 +21,36 @@ $(function () {
         	}
 
         ).done(function (result) {
-        	$("#fromTweets").empty();  
+        	$("#fromTweets").empty();   
+
         	for (i = 0; i < result.statuses.length; i++) {
+
+                //Print out username and status
 				$("#fromTweets").append('<b>' + "Username: " + '</b>' + result.statuses[i].user.screen_name + '<br/>');
         		$("#fromTweets").append('<b>' + "Tweet: " + '</b>' + result.statuses[i].text + '<br/>');
 
         		if (result.statuses[i].geo !== null) {
+                    //Print out the geolocation
 					$("#fromTweets").append('<b>' + "GeoLocation: " + '</b>' + "Lat: " + result.statuses[i].geo.coordinates[0] + " Lon: " + result.statuses[i].geo.coordinates[1] + '<br/>'+ '<br/>');
+
+                    //dropping a new marker on the map for each tweet that has lat/lon values
+                    var newMarker = "marker" + i;
+                    LatValue = parseFloat(result.statuses[i].geo.coordinates[0]);
+                    LonValue = parseFloat(result.statuses[i].geo.coordinates[1]);
+                    myLatLng = {lat: LatValue, lng: LonValue};
+
+                    newMarker = new google.maps.Marker({
+                        position: myLatLng,
+                        map: map,
+                        title: 'Hello World!'
+                    });
+
+                    console.log(LatValue + " " + LonValue);
         		} else {
         			$("#fromTweets").append('<b>' + "GeoLocation: " + '</b>' + "Cannot be identified" + '<br/>' + '<br/>')
         		}
         	}
         });
-
-
-
-        // Drop marker on user's current location in Map
-        var LatValue = parseFloat(document.getElementById( "searchLat" ).value);
-        var LonValue = parseFloat(document.getElementById( "searchLon").value);
-        var myLatLng = {lat:  LatValue, lng: LonValue};
-
-        var map = new google.maps.Map(document.getElementById('map-canvas'), {
-            zoom: 12,
-            center: myLatLng
-        });
-
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Hello World!'
-        });
-        // End Map actions
-
     });
 
 
