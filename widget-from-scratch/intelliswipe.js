c64f6f131466b71d0f6b0a965ab8ca91e6e86a2a
Victor Frolov <vicfrolov@gmail.com>
2015-12-11 03:59:37 -0800

    implemented overflow hidden so panels appear to disappear into the container. added a @media override where text seemed to grow bigger because of this overflow. Added a link to demo li element, and some css changes to demo

--- widget-from-scratch/intelliswipe.js
+++ widget-from-scratch/intelliswipe.js
@@ -4,21 +4,9 @@
         element.remove();
     }
 
-    var startMove = function (event) {
-        $.each(event.changedTouches, function (index, touch) {
-            var jThis = $(touch.target),
-                startOffset = jThis.offset();
-
-            touch.target.movingBox = jThis;
-            touch.target.deltaX = touch.pageX - startOffset.left;
-
-        });
-        event.stopPropagation();
-    }
-
     var trackSlide = function (event) {
         $.each(event.changedTouches, function (index, touch) {
-            if (touch.target.movingBox) {         
+            if (touch.target.movingBox) {
                 touch.target.movingBox.offset({
                     left: touch.pageX - touch.target.deltaX
                 });
@@ -28,11 +16,33 @@
         event.preventDefault();
     }
 
+    var endSlide = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            if (touch.target.movingBox) {
+                touch.target.movingBox = null;
+            }
+        });
+    } 
+
+    var startMove = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+
+            var jThis = $(touch.target),
+                startOffset = jThis.offset();
+            console.log(jThis);
+            touch.target.movingBox = jThis;
+            touch.target.deltaX = touch.pageX - startOffset.left;
+        });
+        event.stopPropagation();
+    }       
+
     $.fn.intelliswipe = function () {
         var children = $(this).children();
         children.each(function (index, element) {
             element.addEventListener("touchstart", startMove, false);
             element.addEventListener("touchmove", trackSlide, false);
+            element.addEventListener("touchend", endSlide, false);
+
         });
     };
 }(jQuery));
