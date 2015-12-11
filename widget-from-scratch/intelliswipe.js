f5a4c5c8bbd86b30bf4f8aa85ff2ed96127fb96c
Victor Frolov <vicfrolov@gmail.com>
2015-12-11 02:31:42 -0800

    implemented trackslide and startmove functions, made html responsive to mobile devices

--- widget-from-scratch/intelliswipe.js
+++ widget-from-scratch/intelliswipe.js
@@ -1,21 +1,38 @@
 (function ($) {
 
-    var LEFT_BUTTON = 1; 
-
-
     var deleteTweet = function (element) {
         element.remove();
     }
 
+    var startMove = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            var jThis = $(touch.target),
+                startOffset = jThis.offset();
+
+            touch.target.movingBox = jThis;
+            touch.target.deltaX = touch.pageX - startOffset.left;
+
+        });
+        event.stopPropagation();
+    }
+
+    var trackSlide = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            if (touch.target.movingBox) {         
+                touch.target.movingBox.offset({
+                    left: touch.pageX - touch.target.deltaX
+                });
+                touch.target.lastX = touch.pageX;
+            }
+        });
+        event.preventDefault();
+    }
 
     $.fn.intelliswipe = function () {
         var children = $(this).children();
-
-        children.each(function(child) {
-            $(this).click(function(){
-                deleteTweet($(this));
-            });
+        children.each(function (index, element) {
+            element.addEventListener("touchstart", startMove, false);
+            element.addEventListener("touchmove", trackSlide, false);
         });
-
     };
 }(jQuery));
