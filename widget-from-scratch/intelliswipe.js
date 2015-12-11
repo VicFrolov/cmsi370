0b86a6fbb3db9542e08431190e2cff9afb3cece7
Victor Frolov <vicfrolov@gmail.com>
2015-12-10 23:24:41 -0800

    delete an li element on click, added some li elements to HTML, called

--- widget-from-scratch/intelliswipe.js
+++ widget-from-scratch/intelliswipe.js
@@ -1,6 +1,21 @@
 (function ($) {
 
+    var LEFT_BUTTON = 1; 
+
+
+    var deleteTweet = function (element) {
+        element.remove();
+    }
+
+
     $.fn.intelliswipe = function () {
+        var children = $(this).children();
+
+        children.each(function(child) {
+            $(this).click(function(){
+                deleteTweet($(this));
+            });
+        });
 
     };
 }(jQuery));
